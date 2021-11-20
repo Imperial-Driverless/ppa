16dc8a209b9292f7e3fabfeb0a58087a9d94fdaa
Mateusz Lichota <mateusz@lichota.net>
2021-11-20 16:03:35 +0000

    add a script generating the rosdep-index package

---
+++ build_rosdep_index_package.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+FILENAME=50-imperial-driverless.list
+PACKAGE_NAME=imperial-driverless-rosdep-index
+VERSION=0.0.0
+DEB_FILE=${PACKAGE_NAME}_${VERSION}-0focal_amd64.deb
+
+echo yaml https://imperial-driverless.github.io/ppa/rosdep.yaml > $FILENAME
+
+rm -f $DEB_FILE
+
+fpm --input-type dir \
+    --output-type deb \
+    --name $PACKAGE_NAME \
+    --version $VERSION \
+    --architecture all \
+    --deb-priority required \
+    -m "Imperial Driverless <imperial.driverless@gmail.com>" \
+    --package $DEB_FILE \
+    $FILENAME=/etc/ros/rosdep/sources.list.d/
+
+rm $FILENAME
