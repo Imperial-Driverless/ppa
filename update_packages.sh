6664fc073b491ccf8f75ae9cb3ccc3fe48498185
Mateusz Lichota <mateusz@lichota.net>
2021-11-20 17:14:13 +0000

    automatically generate the package list in README

--- update_packages.sh
+++ update_packages.sh
@@ -7,3 +7,9 @@ gzip -k -f Packages &&
 apt-ftparchive release . > Release &&
 gpg --default-key "${EMAIL}" -abs -o - Release > Release.gpg &&
 gpg --default-key "${EMAIL}" --clearsign -o - Release > InRelease
+
+
+sed '/### Currently available packages/q' README.md > README.md.tmp
+sed -n 's/^Package:/- /p' Packages | sort | uniq >> README.md.tmp
+mv README.md.tmp README.md
+
