306eca195d6892f49c1d5c34764a333153da8b7a
Mateusz Lichota <mateusz@lichota.net>
2021-12-03 00:11:56 +0000

    release imperial_driverless_utils 0.0.0

--- build_rosdep_index_package.sh
+++ build_rosdep_index_package.sh
@@ -2,7 +2,7 @@
 
 FILENAME=50-imperial-driverless.list
 PACKAGE_NAME=imperial-driverless-rosdep-index
-VERSION=0.5.0
+VERSION=$1
 DEB_FILE=packages/${PACKAGE_NAME}_${VERSION}-0focal_amd64.deb
 
 echo yaml https://imperial-driverless.github.io/ppa/rosdep.yaml > $FILENAME
